6b873b839ae972c5ff26829a329ff92cd0b5ab1e
Cullen Jennings <fluffy@iii.ca>
2013-12-27 14:52:11 -0700

    non coherent energy detection

--- dsp.cpp
+++ dsp.cpp
@@ -8,38 +8,37 @@
 
 static int count=0;
 
-void genTestData( int sampleRate, int numSamples, double* samples,
-                  double symbolTime, double transitionTime, double frequency,
-                  double startTime, int numSym );
 
-
-
-
-
-int soundProcess( int sampleRate, int numSamples, double* samples, 
-                  double symbolTime, double transitionTime, double frequency,
-                  double* refSig )
+double nonCoherentEnergy( double freq, double sampleRate, double samples[], int start, int end) 
 {
-   int numSymbols = 16;
-   char patern[] = "pnpp";
-   int paternLen = 4;
-
-   if ( count == 0 )
-   {
-      printf( "In soundProcess %d Hz, %d Samples \n", sampleRate, numSamples );
-      genTestData( sampleRate, numSamples, samples, 
-                   symbolTime, transitionTime, frequency,
-                   0.100 /*startTime*/, numSymbols );
-      count++;
-      printf( "Gen fake data" );
-   }
-   
-   double min = 1e22;
-   double max = -1e22;
-
-   return 0;
+    assert( freq > 0.0 );
+    
+    float waveLength = sampleRate / freq;
+    
+    // Goertzel filter
+    double omega = 2.0 * M_PI / waveLength;
+    double sinOmega = sin( omega );
+    double cosOmega = cos( omega );
+    double c = 2.0 * cosOmega;
+    
+    double u = 0.0;
+    double uPrev = 0.0;
+    double uPrevPrev = 0.0;
+    
+    for( int i=start; i<end; i++ )
+    {
+        u = c * uPrev - uPrevPrev + samples[i];
+        uPrevPrev = uPrev;
+        uPrev = u;
+    }
+    
+    double i = (uPrev - uPrevPrev * cosOmega ) * 2.0 / float(end-start);
+    double q = (uPrevPrev * sinOmega ) * 2.0 / float(end-start);
+    
+    double energy = sqrt( i*i + q*q );
+    
+    return energy;
 }
-
 
 
 void genTestData( int sampleRate, int numSamples, double* samples,
@@ -83,4 +82,57 @@ void genTestData( int sampleRate, int numSamples, double* samples,
         samples[i] = dataI;
     }
 }
+
+
+int soundProcess( int sampleRate, int numSamples, double* samples, 
+                  double symbolTime, double transitionTime, double frequency,
+                  double* refSig )
+{
+   int numSymbols = 16;
+   char patern[] = "pnpp";
+   int paternLen = 4;
+
+   if ( count == 0 )
+   {
+      printf( "In soundProcess %d Hz, %d Samples \n", sampleRate, numSamples );
+      genTestData( sampleRate, numSamples, samples, 
+                   symbolTime, transitionTime, frequency,
+                   0.100 /*startTime*/, numSymbols );
+      count++;
+      printf( "Gen fake data" );
+   }
+   
+   double min = 1e22;
+   double max = -1e22;
+
+   int chipSize = int( round( double(sampleRate) * symbolTime ) );
+   
+
+   for ( int i=0; i < numSamples-chipSize ; i += chipSize )
+   {
+      double e = nonCoherentEnergy( frequency, sampleRate, samples, i, i+chipSize );
+
+      if ( e > 0 )
+      {
+         if ( e < min ) min = e;
+         if ( e > max ) max = e;
+      }
+   }
+   
+   double snr = 0.0;
+   if ( min > 0.0 )
+   {
+      snr = 10.0 * log10( max / min );
+   }
+   printf("SNR = %lf dB  max=%lf \n", snr , 10.0 * log10( max  ) );
+
+   if ( snr < 25 )
+   {
+      return 1;
+   }
+
+   return 0;
+}
+
+
 
