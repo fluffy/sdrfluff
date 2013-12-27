634b724202f782592408b26470a24e0381af8a71
Cullen Jennings <fluffy@iii.ca>
2013-12-27 11:57:58 -0700

    create namespace

--- sdrFluff.js
+++ sdrFluff.js
@@ -1,1 +1,22 @@
 
+var Fluffy = Fluffy || {}; // create namespace
+
+Fluffy.SDR = function()
+{
+    // private functions
+
+    // public functions
+    var init = function()
+    {
+    };
+
+    var publicExport =
+        {
+            init: init
+        };
+
+    return publicExport;
+}();
+
+Fluffy.SDR.init();
+
